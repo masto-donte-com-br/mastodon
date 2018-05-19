e855293296f226dbba1fd45808ea5f644d8e32f6
chendo <chendo@users.noreply.github.com>
2018-05-19 20:38:54 +1000

    Popular v1.1

--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -2,8 +2,24 @@ class PopularController < ApplicationController
   layout 'public'
 
 
+  REBLOG_WEIGHTING = 1
+  FAVOURITE_WEIGHTING = 1
+  CONVERSATION_WEIGHTING = 2
   def index
-    @popular = Status.local.with_public_visibility.where('reblogs_count + favourites_count > ?', threshold).where('created_at BETWEEN ? AND ?', page.days.ago, (page - 1).days.ago).limit(50).reorder('reblogs_count + favourites_count DESC')
+    @popular = Status
+                .local
+                .includes(:account, :preview_cards, :mentions, :conversation)
+                .with_public_visibility
+                .where('reblogs_count + favourites_count > ?', threshold)
+                .where('created_at BETWEEN ? AND ?', page.days.ago, (page - 1).days.ago)
+                .reorder('reblogs_count + favourites_count DESC')
+                .limit(25)
+                .sort_by { |s|
+                  s.reblogs_count * REBLOG_WEIGHTING +
+                  s.favourites_count * FAVOURITE_WEIGHTING +
+                  s.conversation.statuses.count * CONVERSATION_WEIGHTING
+                }
+                .reverse
     @page = page
   end
 
