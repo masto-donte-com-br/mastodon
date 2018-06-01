0f86e77ec7541fde9522c28c4d8a49c9d7d8a802
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-06-01 09:39:13 +0200

    Add popular page for embassador bot
    Squashed commit of the following:
    
    commit 0e64473b543c806e70317d57c1f5bc55af9cf948
    Author: Renato "Lond" Cerqueira <renato@lond.com.br>
    Date:   Fri Jun 1 09:29:39 2018 +0200
    
        Use same query as in prod, authorize only for ambassador
    
    commit d3fba290c624ac6d63f6e53bd5e12b37abeec5b3
    Author: Renato "Lond" Cerqueira <renato@lond.com.br>
    Date:   Thu May 31 21:50:23 2018 +0200
    
        Revert change to detailed status, create new template instead
    
    commit e051add76379f9c89b5b9ea3d7e11eae29ee0d3a
    Author: chendo <chendo@users.noreply.github.com>
    Date:   Sat May 19 20:58:57 2018 +1000
    
        Popular v1.2 - link conversation count and remove reblogs from conversation count
    
    commit 17bf69b259fc781fd1ec2b094fa45f9e2be47488
    Author: chendo <chendo@users.noreply.github.com>
    Date:   Sat May 19 20:38:54 2018 +1000
    
        Popular v1.1
    
    commit 09eeb40d3577499f03de66537ba0f5c53b116cfa
    Author: chendo <chendo@users.noreply.github.com>
    Date:   Sat May 19 19:52:40 2018 +1000
    
        Popular v1

---
+++ app/controllers/popular_controller.rb
@@ -0,0 +1,31 @@
+class PopularController < ApplicationController
+  include Authorization
+  layout 'public'
+
+  EXCLUDED_ACCOUNT_IDS = ENV['EXCLUDED_ACCOUNT_IDS'] || [-1]
+  def index
+    authorize :popular, :index?
+    @popular = Status.
+                local.
+                with_public_visibility.
+                where('reblogs_count + favourites_count > ?', threshold).
+                where(%q(created_at > NOW() - INTERVAL '7 days')).
+                limit(100).
+                reverse
+    @page = page
+  end
+
+  protected
+
+  def threshold
+    base = Status.local.with_public_visibility.where('account_id not in (?)', EXCLUDED_ACCOUNT_IDS).where('favourites_count > 1 or reblogs_count > 1').where(%q(created_at > NOW() - INTERVAL '30 days'))
+    favorites = base.average('statuses.favourites_count') || 0
+    reblogs = base.average('statuses.reblogs_count') || 0
+    favorites + reblogs
+  end
+
+  def page
+    (params[:page].presence || 1).to_i
+  end
+
+end
