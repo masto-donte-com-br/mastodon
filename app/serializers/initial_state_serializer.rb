0918a33e8f570bbbb8a413f068cf58628d8505da
Renato "Lond" Cerqueira <renato@lond.com.br>
2020-08-04 23:23:03 +0200

    Activate advanced invite rules
    Allow older users to invite up to a maximum of people in a given
    interval of time.

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -32,6 +32,7 @@ class InitialStateSerializer < ActiveModel::Serializer
       store[:use_pending_items] = object_account_user.setting_use_pending_items
       store[:show_trends]       = Setting.trends && object_account_user.setting_trends
       store[:emoji_style]       = object_account_user.settings['web.emoji_style']
+      store[:invites_enabled]   = InvitePolicy.new(object.current_account, nil).create?
     else
       store[:auto_play_gif] = Setting.auto_play_gif
       store[:display_media] = Setting.display_media
